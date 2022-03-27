1d31d708864a568ebd40e6daf839e0118b317c0c
Sagar Thakur <86216787+huntersagar@users.noreply.github.com>
2022-03-27 12:29:03 +0545

    Update cpp_ide.cpp

--- cpp_ide.cpp
+++ cpp_ide.cpp
@@ -3,12 +3,16 @@
 using namespace std;
 fstream out;
 int main(){
-	string x;
-	cout<<"Write Your C code here"<<endl<<"[+]When your program is finished click \'=\' and press enter to save it<<endl;
-	getline(cin,x,'=');
-    out.open("file.c",ios::in|ios::out|ios::trunc);
+    string x;
+    cout<<"Write Your C code here"<<endl<<"[+]When your program is finished click \'\\\$=\' and press enter to save it<<endl;
+    getline(cin,x,'=');
+    out.open("file.c++",ios::out|ios::trunc);
     out<<x;
-    system("gcc file.c -o file.exe");
+    out.close();
+    out.open("file.cpp", ios::in);
+    getline(out,x,'$');
+    system("G++ file.pp -o file.exe");
     system("./file.exe");
+    out.close();
     return 0;
 }
