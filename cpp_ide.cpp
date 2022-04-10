4ab9a38f25be9d3210c77e4b28be222acbb1b65b
Sagar Thakur <86216787+huntersagar@users.noreply.github.com>
2022-04-10 16:24:49 +0545

    Update cpp_ide.cpp

--- cpp_ide.cpp
+++ cpp_ide.cpp
@@ -4,14 +4,14 @@ using namespace std;
 fstream out;
 int main(){
     string x;
-    cout<<"Write Your C code here"<<endl<<"[+]When your program is finished click \'\\\$=\' and press enter to save it<<endl;
+    cout<<"Write Your C++ code here"<<endl<<"[+]When your program is finished click \'\\\$=\' and press enter to save it<<endl;
     getline(cin,x,'=');
-    out.open("file.c++",ios::out|ios::trunc);
+    out.open("file.cpp",ios::out|ios::trunc);
     out<<x;
     out.close();
     out.open("file.cpp", ios::in);
     getline(out,x,'$');
-    system("G++ file.pp -o file.exe");
+    system("G++ file.cpp -o file.exe");
     system("./file.exe");
     out.close();
     return 0;
